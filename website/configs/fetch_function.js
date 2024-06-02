9e32f54640f8d6f419d6a624e6faa703ae09b29b
ikanko1989 <iikanko1989@gmail.com>
2024-06-02 11:34:23 +0200

    added .github and configs

---
+++ website/configs/fetch_function.js
@@ -0,0 +1,11 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("arn:aws:lambda:us-east-1:767397953558:function:updateDatabaseCounter");
+    let data = await response.json();
+    counter.innerHTML = ` This page has ${data} Views!`;
+}
+
+updateCounter();
+
+
+
